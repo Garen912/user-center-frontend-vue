39e2a345b0979be8f784fdf14259b097996b5084
pang912 <936046325@qq.com>
2025-01-04 19:25:48 +0800

    feat: 新增用户注册页

--- src/pages/user/UserRegisterPage.vue
+++ src/pages/user/UserRegisterPage.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="userRegisterPage">
     <h2 class="title">用户注册</h2>
-
     <a-form
       style="max-width: 480px; margin: 0 auto"
       :model="formState"
@@ -14,35 +13,52 @@
     >
       <a-form-item
         label="账号"
-        name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
+        name="userAccount"
+        :rules="[{ required: true, message: '请输入账号' }]"
       >
-        <a-input v-model:value="formState.username" />
+        <a-input
+          v-model:value="formState.userAccount"
+          placeholder="请输入账号"
+        />
       </a-form-item>
       <a-form-item
         label="密码"
-        name="password"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
+        name="userPassword"
+        :rules="[
+          { required: true, message: '请输入密码' },
+          { min: 8, message: '密码不能小于 8 位' },
+        ]"
       >
-        <a-input v-model:value="formState.username" />
+        <a-input-password
+          v-model:value="formState.userPassword"
+          placeholder="请输入密码"
+        />
       </a-form-item>
       <a-form-item
         label="确认密码"
-        name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
+        name="checkPassword"
+        :rules="[
+          { required: true, message: '请输入确认密码' },
+          { min: 8, message: '确认密码不能小于 8 位' },
+        ]"
       >
-        <a-input v-model:value="formState.username" />
+        <a-input-password
+          v-model:value="formState.checkPassword"
+          placeholder="请输入确认密码"
+        />
       </a-form-item>
       <a-form-item
         label="编号"
-        name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
+        name="planetCode"
+        :rules="[{ required: true, message: '请输入编号' }]"
       >
-        <a-input v-model:value="formState.username" />
+        <a-input
+          v-model:value="formState.planetCode"
+          placeholder="请输入编号"
+        />
       </a-form-item>
-
-      <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-        <a-button type="primary" html-type="submit">Submit</a-button>
+      <a-form-item :wrapper-col="{ offset: 4, span: 20 }">
+        <a-button type="primary" html-type="submit">注册</a-button>
       </a-form-item>
     </a-form>
   </div>
