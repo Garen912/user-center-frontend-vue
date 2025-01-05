1bbc2f2f3feca5ca2cf8fb8ad8965bdc14f38c97
pang912 <936046325@qq.com>
2025-01-05 15:09:19 +0800

    build: 去掉vue的标识警告

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,14 @@ module.exports = defineConfig({
       port: 3300,
     },
   },
+  chainWebpack: (config) => {
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+      })
+      return definitions
+    })
+  }
 })
