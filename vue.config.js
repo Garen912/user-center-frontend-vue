dc862c98a7bf5e70ff3a749529c0d2676bb24b3f
Garen912 <936046325@qq.com>
2025-01-10 00:16:08 +0800

    feat: 设置网页标题&添加请求代理配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const name = '用户中心' // page title
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
@@ -8,9 +11,20 @@ module.exports = defineConfig({
     devtool: 'source-map', // 使用 source-map
     devServer: {
       port: 3300,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080', // 目标服务器的地址
+          changeOrigin: true, // 是否改变请求源
+          // pathRewrite: { '^/api': '' }, // 重写路径
+        },
+      }
     },
   },
   chainWebpack: (config) => {
+    config.plugin('html').tap(args => {
+      args[0].title = name // 设置网页标题
+      return args;
+    })
     config.plugin('define').tap((definitions) => {
       Object.assign(definitions[0], {
         __VUE_OPTIONS_API__: 'true',
